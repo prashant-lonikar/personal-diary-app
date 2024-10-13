ec16e7900b96858a1b2616e123e8953f9e75a25c
Prashant Lonikar <prashant.lonikar@gmail.com>
2024-10-13 16:04:27 +0100

    Add password functionality and ability to edit password after login

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Login({ onLogin }) {
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(password);
+  };
+
+  return (
+    <div className="login">
+      <h2>Enter Password</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
